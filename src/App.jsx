a194600cd62bc4e927126ee41215147fc73eb366
Linderlly  Santana <148879489+Linderlly@users.noreply.github.com>
2026-05-28 10:20:08 -0400

    Correção na falha ao carregar a página devido ao tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,105 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route
+} from "react-router-dom"
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Tournament from "./pages/Tournament";
-import Admin from "./pages/Admin";
-import JoinTournament from "./pages/JoinTournament";
-import Profile from "./pages/Profile";
-import Leaderboard from "./pages/Leaderboard";
-import AdminRoute from "./routes/AdminRoute";
-import MyTournaments from "./pages/MyTournaments";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+import Tournament from "./pages/Tournament"
+import Admin from "./pages/Admin"
+import JoinTournament from "./pages/JoinTournament"
+import Profile from "./pages/Profile"
+import Leaderboard from "./pages/Leaderboard"
+import MyTournaments from "./pages/MyTournaments"
 
-import PrivateRoute from "./routes/PrivateRoute";
+import AdminRoute from "./routes/AdminRoute"
+import PrivateRoute from "./routes/PrivateRoute"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* LOGIN */}
-        <Route path="/" element={<Login />} />
+    <Routes>
+      {/* LOGIN */}
+      <Route
+        path="/"
+        element={<Login />}
+      />
 
-        {/* REGISTER */}
-        <Route path="/register" element={<Register />} />
+      {/* REGISTER */}
+      <Route
+        path="/register"
+        element={<Register />}
+      />
 
-        {/* DASHBOARD */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+      {/* DASHBOARD */}
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
 
-        {/* ADMIN */}
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <Admin />
-            </AdminRoute>
-          }
-        />
+      {/* ADMIN */}
+      <Route
+        path="/admin"
+        element={
+          <AdminRoute>
+            <Admin />
+          </AdminRoute>
+        }
+      />
 
-        {/* TOURNAMENT */}
-        <Route
-          path="/tournament/:id"
-          element={
-            <PrivateRoute>
-              <Tournament />
-            </PrivateRoute>
-          }
-        />
+      {/* TOURNAMENT */}
+      <Route
+        path="/tournament/:id"
+        element={
+          <PrivateRoute>
+            <Tournament />
+          </PrivateRoute>
+        }
+      />
 
-        {/* JOIN */}
-        <Route
-          path="/join"
-          element={
-            <PrivateRoute>
-              <JoinTournament />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/leaderboard"
-          element={
-            <PrivateRoute>
-              <Leaderboard />
-            </PrivateRoute>
-          }
-        />
+      {/* JOIN */}
+      <Route
+        path="/join"
+        element={
+          <PrivateRoute>
+            <JoinTournament />
+          </PrivateRoute>
+        }
+      />
 
-        <Route
-          path="/my-tournaments"
-          element={
-            <PrivateRoute>
-              <MyTournaments />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+      {/* PROFILE */}
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+
+      {/* LEADERBOARD */}
+      <Route
+        path="/leaderboard"
+        element={
+          <PrivateRoute>
+            <Leaderboard />
+          </PrivateRoute>
+        }
+      />
+
+      {/* MY TOURNAMENTS */}
+      <Route
+        path="/my-tournaments"
+        element={
+          <PrivateRoute>
+            <MyTournaments />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
-    extend: {
-      colors: {
-        neon: '#00F5FF',
-        gamer: '#0f172a',
-        victory: '#22c55e',
-        danger: '#ef4444',
-        gold: '#FFD700',
-      },
-      boxShadow: {
-        neon: '0 0 15px #00F5FF',
-        gold: '0 0 25px #FFD700',
-      }
-    },
+    extend: {},
   },
+
   plugins: [],
 }
